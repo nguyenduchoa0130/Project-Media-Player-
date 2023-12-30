42df62866f47b6262abda06b9896c018a5561a0f
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2023-12-30 21:32:57 +0700

    UI upload file

---
+++ MediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace MediaPlayer
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonOpenMenu_Click(object sender, RoutedEventArgs e)
+		{
+			buttonCloseMenu.Visibility = Visibility.Visible;
+			buttonOpenMenu.Visibility = Visibility.Collapsed;
+			menuWidth!.Width = new GridLength(200);
+		}
+
+		private void buttonCloseMenu_Click(object sender, RoutedEventArgs e)
+		{
+			buttonCloseMenu.Visibility = Visibility.Collapsed;
+			buttonOpenMenu.Visibility = Visibility.Visible;
+			menuWidth.Width = new GridLength(60);
+		}
+
+		private void listViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			GridMain.Children.Clear();
+
+			switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+			{
+				case "MyMusicItem":
+					GridMain.Children.Add(new MyMusicUserControl());
+					break;
+				case "RecentPlaysItem":
+					GridMain.Children.Add(new RecentPlaysUserControl());
+					break;
+				case "PlaylistsItem":
+					GridMain.Children.Add(new PlaylistsUserControl());
+					break;
+				default:
+					GridMain.Children.Add(new MyMusicUserControl());
+					break;
+			}
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			GridMain.Children.Clear();
+			GridMain.Children.Add(new MyMusicUserControl());
+		}
+	}
+}
---
+++ MediaPlayer/MyMusicUserControl.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Path = System.IO.Path;
+
+namespace MediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MyMusicUserControl.xaml
+    /// </summary>
+    public partial class MyMusicUserControl : UserControl
+    {
+        public MyMusicUserControl()
+        {
+            InitializeComponent();
+        }
+
+		public class Object : INotifyPropertyChanged
+		{
+			public string name;
+			public string dir;
+			public string extension;
+			public string Name
+			{
+				get => name; set
+				{
+					name = value;
+					RaiseEvent();
+				}
+			}
+			public string Extension
+			{
+				get => extension; set
+				{
+					extension = value;
+					RaiseEvent();
+				}
+			}
+			public string Dir
+			{
+				get => dir; set
+				{
+					dir = value;
+					RaiseEvent();
+				}
+			}
+			public event PropertyChangedEventHandler PropertyChanged;
+
+			void RaiseEvent([CallerMemberName] string propertyName = "")
+			{
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		ObservableCollection<Object> Objects = new ObservableCollection<Object>();
+
+		private void addMusicFile_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+			dialog.Multiselect = true;
+
+			if (dialog.ShowDialog() == true)
+			{
+				foreach (string sFileName in dialog.FileNames)
+				{
+					Objects.Add(new Object { Name = Path.GetFileName(sFileName), Dir = Path.GetDirectoryName(sFileName) + "\\", Extension = Path.GetExtension(sFileName) });
+					for (int i = 0; i < Objects.Count; i++)
+					{
+						for (int j = i + 1; j < Objects.Count; j++)
+						{
+							if (Objects[i].Name == Objects[j].Name)
+								Objects.Remove(Objects[j]);
+						}
+					}
+
+				}
+
+				musicListView.Items.Clear();
+				foreach (Object obj in Objects)
+				{
+					musicListView.Items.Add(obj);
+				}
+			}
+
+		}
+
+	}
+}
